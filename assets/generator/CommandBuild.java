9a2cc1ce201780fd1af3d139823f1ad709f60a36
GotoLink <gotolinkminecraft@gmail.com>
2015-06-13 00:46:37 +0200

    Improved build command

--- assets/generator/CommandBuild.java
+++ assets/generator/CommandBuild.java
@@ -51,42 +51,39 @@ public final class CommandBuild extends CommandBase {
 
 	@Override
 	public void processCommand(ICommandSender var1, String[] coordinate) {
-		if (coordinate.length == 4 || coordinate.length == 3) {
-            func_152373_a(var1, this, "/build command used by " + var1.getCommandSenderName(), var1.getCommandSenderName(), coordinate);
+		if (coordinate.length > 2) {
 			int posX = parseInt(var1, coordinate[coordinate.length - 2]);
 			int posZ = parseInt(var1, coordinate[coordinate.length - 1]);
 			World world = DimensionManager.getWorld(coordinate.length == 3 ? 0 : Integer.parseInt(coordinate[1]));
-			if ("ruin".equalsIgnoreCase(coordinate[0])) {
-				(new WorldGenCARuins(PopulatorCARuins.instance, world, new Random(), posX, posZ, 1, 1)).run();
-			} else if ("wall".equalsIgnoreCase(coordinate[0])) {
-				(new WorldGenGreatWall(PopulatorGreatWall.instance, world, new Random(), posX, posZ, 1, 1)).run();
+			if(world!=null) {
+				WorldGeneratorThread wgt = null;
+				long worldSeed = world.getSeed();
+				Random random = new Random(worldSeed);
+				long xSeed = random.nextLong() >> 2 + 1L;
+				long zSeed = random.nextLong() >> 2 + 1L;
+				random.setSeed((xSeed * (posX / 16) + zSeed * (posZ / 16)) ^ worldSeed);
+				if ("ruin".equalsIgnoreCase(coordinate[0])) {
+					wgt = (new WorldGenCARuins(PopulatorCARuins.instance, world, random, posX, posZ, 10, 1));
+				} else if ("wall".equalsIgnoreCase(coordinate[0])) {
+					wgt = (new WorldGenGreatWall(PopulatorGreatWall.instance, world, random, posX, posZ, 10, 1));
+				} else if ("city".equalsIgnoreCase(coordinate[0])) {
+					wgt = (new WorldGenWalledCity(PopulatorWalledCity.instance, world, random, posX, posZ, 100, 1));
+				} else if ("undcity".equalsIgnoreCase(coordinate[0])) {
+					PopulatorWalledCity city = PopulatorWalledCity.instance;
+					wgt = new WorldGenUndergroundCity(city, world, random, posX, posZ, 10, 1);
+					int maxSpawnHeight = Building.findSurfaceJ(world, posX, posZ, Building.WORLD_MAX_Y, false, Building.IGNORE_WATER) - WorldGenUndergroundCity.MAX_DIAM / 2 - 5; //44 at sea level
+					int minSpawnHeight = PopulatorWalledCity.MAX_FOG_HEIGHT + WorldGenUndergroundCity.MAX_DIAM / 2 - 8; //34, a pretty thin margin. Too thin for underocean cities?
+					if (minSpawnHeight <= maxSpawnHeight)
+						wgt.setSpawnHeight(minSpawnHeight, maxSpawnHeight, false);
+				}
+				if(wgt!=null){
+					wgt.run();
+					func_152373_a(var1, this, "/build command used by " + var1.getCommandSenderName());
+					return;
+				}
 			}
-			/*
-			 * else if ("monowall".equalsIgnoreCase(coordinate[0])) {
-			 * wall=PopulatorGreatWall.instance; if(wall.placedCoords==null ||
-			 * wall.placedWorld!=world){ wall.placedCoords=new
-			 * int[]{posX,world.getHeightValue(posX, posZ),posZ};
-			 * wall.placedWorld=world;
-			 * var1.sendChatToPlayer("First set of wall coordinates stored."); }
-			 * else{ wall.master.exploreThreads.add(new WorldGenSingleWall(
-			 * wall, world, world.rand, new int[]{posX,
-			 * world.getHeightValue(posX, posZ), posZ}));
-			 * wall.placedCoords=null; wall.placedWorld=null; } }
-			 */
-			else if ("city".equalsIgnoreCase(coordinate[0])) {
-				(new WorldGenWalledCity(PopulatorWalledCity.instance, world, new Random(), posX, posZ, 1, 1)).run();
-			} else if ("undcity".equalsIgnoreCase(coordinate[0])) {
-				PopulatorWalledCity city = PopulatorWalledCity.instance;
-				WorldGeneratorThread wgt = new WorldGenUndergroundCity(city, world, new Random(), posX, posZ, 1, 1);
-				int maxSpawnHeight = Building.findSurfaceJ(world, posX, posZ, Building.WORLD_MAX_Y, false, Building.IGNORE_WATER) - WorldGenUndergroundCity.MAX_DIAM / 2 - 5; //44 at sea level
-				int minSpawnHeight = PopulatorWalledCity.MAX_FOG_HEIGHT + WorldGenUndergroundCity.MAX_DIAM / 2 - 8; //34, a pretty thin margin. Too thin for underocean cities?
-				if (minSpawnHeight <= maxSpawnHeight)
-					wgt.setSpawnHeight(minSpawnHeight, maxSpawnHeight, false);
-				(wgt).run();
-			}
-		} else {
-			throw new WrongUsageException(getCommandUsage(var1));
 		}
+		throw new WrongUsageException(getCommandUsage(var1));
 	}
 
 	@Override
